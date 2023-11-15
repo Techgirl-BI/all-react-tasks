19fa85dce7615b1d0cf393faf5d02e45af3fac5a
Techgirl-FE <rachealonimisi31@gmail.com>
2023-11-15 18:56:51 -0100

    second commit

--- src/BabyNames/MainApp.js
+++ src/BabyNames/MainApp.js
@@ -1,21 +1,26 @@
-import data from './data.json'
-import React from 'react'
+import babydata from './data.json'
+import {useState} from 'react'
 import './babyNames.css'
 
 function MainApp() {
+    const [data, setData] = useState(babydata)
+    const [searchTerm, setSearchTerm] = useState("")
+    const handleFilter = (e) => {
+        setSearchTerm(e.target.value)
+        const newdata = data.filter((names) => names.includes(searchTerm))
+        setData(newdata)
+    }
   return (
    <div className='main-container'>
+    <input onChange={()=>handleFilter()} type='search' placeholder='Search for a name...' value={searchTerm}/>
    <div className='names-displayed'>
-    {data.map((names) => {
+    {data.sort((a,b) => a.name.localeCompare(b.name)).map((names) => {
          const styles = {
-            backgroundColor: names.sex === 'f'? "pink" : 'dodgerblue',
-            border: 'none',
-            
+            backgroundColor: names.sex === 'f'? "pink" : 'dodgerblue'
         }
-    return <span key={names.id} style={styles}> {names.name} </span>})}
+    return <p key={names.id} style={styles}> {names.name} </p>})}
    </div>
    </div>
-    
   )
 }
 
